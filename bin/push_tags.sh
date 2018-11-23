b97b18f54afc40b7eccb1eeda8afb4d82844dd50
shun <shun@getto.systems>
2018-11-23 04:05:11 +0000

    version dump : 1.1.1

--- bin/push_tags.sh
+++ bin/push_tags.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git remote add super https://gett-systems:$GITLAB_ACCESS_TOKEN@gitlab.com/getto-systems-labo/rubygems-repository-sequel.git
-git remote add github https://getto-systems:$GITHUB_ACCESS_TOKEN@github.com/getto-systems/rubygems-getto-repository-sequel.git
+git remote add backup https://getto-systems:$GITHUB_ACCESS_TOKEN@github.com/getto-systems/rubygems-getto-repository-sequel.git
 git tag $(cat .release-version)
 git push super HEAD:master --tags
-git push github HEAD:master --tags
+git push backup HEAD:master --tags
--- lib/getto/repository/sequel/version.rb
+++ lib/getto/repository/sequel/version.rb
@@ -1,7 +1,7 @@
 module Getto
   module Repository
     class Sequel
-      VERSION = "1.1.0"
+      VERSION = "1.1.1"
     end
   end
 end
